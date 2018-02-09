3a33982e60552dbce50c557b375a95a2a7c682a1
German Flores <gfloresa@ucsd.edu>
2018-02-08 17:53:38 -0800

    fixed adding friends

--- routes/add.js
+++ routes/add.js
@@ -2,11 +2,14 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
-	console.log(data);
-	var newFriend = res.render('index', {
-		'name': req.query.name,
-		'description': req.query.desciption,
-		'imageURL': 'http://lorempixel.com/400/400/people'
-	});
-	data.friends.push(newFriend);
+
+	var toAdd = {
+		"name": req.query.name,
+		"description": req.query.description,
+		"imageURL": "http://lorempixel.com/400/400/people"
+	}
+
+	console.log(toAdd);
+	data.friends.push(toAdd);
+	res.render("index.handlebars", data);
  }
